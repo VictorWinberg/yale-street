82a602bb03a3f46f79075451146d10966ad2df31
Victor Winberg <victor.m.winberg@gmail.com>
2024-05-20 17:32:08 +0200

    feat(data-grid): auto width (#15)
    * feat(data-grid): auto width
    
    * remove fixed with
    
    * autosize

--- src/views/dashboard/Assignments.tsx
+++ src/views/dashboard/Assignments.tsx
@@ -3,31 +3,23 @@ import { Box, Divider } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
-  { field: 'name', headerName: 'Uppdragsnamn', width: 160 },
-  { field: 'company', headerName: 'Bolag', width: 160 },
-  {
-    field: 'email',
-    headerName: 'Email',
-    width: 230
-  },
+  { field: 'name', headerName: 'Uppdragsnamn' },
+  { field: 'company', headerName: 'Bolag' },
+  { field: 'email', headerName: 'Email' },
   {
     field: 'responsible',
     headerName: 'Ansvarig',
-    width: 160,
     sortable: false,
     valueGetter: (_value, row) => `${row.name || ''}`
   },
   {
     field: 'status',
     headerName: 'Status',
-    width: 120,
     sortable: false
   },
   {
     field: 'fee',
     headerName: 'Arvode',
-    width: 120,
-    flex: 1,
     type: 'number',
     renderCell: ({ value }) =>
       Number(value).toLocaleString('sv-SE', {
@@ -54,6 +46,7 @@ const Assignments = () => (
       rows={rows}
       columns={columns}
       autoPageSize
+      autosizeOnMount
       density="compact"
       sx={{
         mx: -1,
