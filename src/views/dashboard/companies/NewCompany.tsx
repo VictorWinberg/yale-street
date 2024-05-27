72559e242eb8bd9345b4802b77ac1de3f9a3ec41
Victor Winberg <victor.m.winberg@gmail.com>
2024-05-27 20:41:19 +0200

    feat(views): add forms to *new* views

--- src/views/dashboard/companies/NewCompany.tsx
+++ src/views/dashboard/companies/NewCompany.tsx
@@ -1,17 +1,50 @@
 // material-ui
+import { Grid, TextField, Button, Box, Autocomplete } from '@mui/material';
 import Typography from '@mui/material/Typography';
+
+// third party
+import { Formik } from 'formik';
 
 // project imports
 
 // ==============================|| NEW COMPANY PAGE ||============================== //
 
-const NewCompany = () => (
-  <>
-    <Typography variant="h3" gutterBottom>
-      NewCompany
-    </Typography>
-    <Typography variant="body1">Hello, this is the NewCompany page.</Typography>
-  </>
-);
+const NewCompany = () => {
+  return (
+    <>
+      <Typography variant="h3">Lägg till företag</Typography>
+      <Box sx={{ my: 1 }} />
+      <Formik initialValues={{}} onSubmit={console.log}>
+        {({ handleSubmit, isSubmitting }) => (
+          <form noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Bolagsnamn" name="name" type="text" margin="none" />
+              </Grid>
+              <Grid item xs={12}>
+                <Autocomplete
+                  multiple
+                  options={DummyOrganisations}
+                  // MUI "key" prop bug: https://github.com/mui/material-ui/pull/42241
+                  renderInput={(params) => <TextField {...params} label="Organisationsnummer" name="orgnr" type="text" />}
+                  limitTags={2}
+                  disableCloseOnSelect
+                />
+              </Grid>
+            </Grid>
+
+            <Box sx={{ mt: 3 }}>
+              <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
+                Spara
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+const DummyOrganisations = [{ label: '556036-0793' }, { label: '556036-0794' }, { label: '556036-0795' }];
 
 export default NewCompany;
--- src/views/dashboard/contacts/NewContact.tsx
+++ src/views/dashboard/contacts/NewContact.tsx
@@ -1,17 +1,73 @@
 // material-ui
+import { Grid, TextField, Button, Box, Autocomplete } from '@mui/material';
 import Typography from '@mui/material/Typography';
+
+// third party
+import { Formik } from 'formik';
 
 // project imports
 
 // ==============================|| NEW CONTACT PAGE ||============================== //
 
-const NewContact = () => (
-  <>
-    <Typography variant="h3" gutterBottom>
-      NewContact
-    </Typography>
-    <Typography variant="body1">Hello, this is the NewContact page.</Typography>
-  </>
-);
+const NewContact = () => {
+  return (
+    <>
+      <Typography variant="h3">Lägg till kontakt</Typography>
+      <Box sx={{ my: 1 }} />
+      <Formik initialValues={{}} onSubmit={console.log}>
+        {({ handleSubmit, isSubmitting }) => (
+          <form noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <TextField fullWidth label="Förnamn" name="name" type="text" margin="none" />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField fullWidth label="Efternamn" name="employer" type="text" margin="none" />
+              </Grid>
+              <Grid item xs={12}>
+                <Autocomplete
+                  multiple
+                  options={DummyCompanies}
+                  // MUI "key" prop bug: https://github.com/mui/material-ui/pull/42241
+                  renderInput={(params) => <TextField {...params} label="Bolag" name="companies" type="text" />}
+                  limitTags={2}
+                  disableCloseOnSelect
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Befattning" name="fee" type="text" margin="none" />
+              </Grid>
+              <Grid item xs={12}>
+                <Autocomplete
+                  multiple
+                  options={DummyDispatches}
+                  // MUI "key" prop bug: https://github.com/mui/material-ui/pull/42241
+                  renderInput={(params) => <TextField {...params} label="Utskick" name="dispatch" type="text" />}
+                  limitTags={2}
+                  disableCloseOnSelect
+                />
+              </Grid>
+            </Grid>
+
+            <Box sx={{ mt: 3 }}>
+              <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
+                Spara
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+const DummyCompanies = [
+  { label: 'Företaget' },
+  { label: 'BRF Sjöstadshöjden' },
+  { label: 'Bil & Bostad AB' },
+  { label: 'Bostadsrättsföreningen' }
+];
+
+const DummyDispatches = [{ label: 'Utskick Syd' }, { label: 'Utskick Väst' }, { label: 'Utskick Norr' }, { label: 'Utskick Öst' }];
 
 export default NewContact;
