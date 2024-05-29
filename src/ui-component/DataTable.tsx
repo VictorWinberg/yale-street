9d9bee4e15e284b50d73b57580b371ccd626765d
Victor Winberg <victor.m.winberg@gmail.com>
2024-05-29 23:39:14 +0200

    feat(data-table): actions width (#20)
    * feat(data-table): actions width

--- src/ui-component/DataTable.tsx
+++ src/ui-component/DataTable.tsx
@@ -59,6 +59,7 @@ const Actions = (apiRef: ReturnType<typeof useGridApiRef>): GridColDef => {
     type: 'actions',
     headerName: 'Actions',
     cellClassName: 'actions',
+    minWidth: 100,
     getActions: ({ id }) => {
       const { getRowMode, startRowEditMode, stopRowEditMode } = apiRef.current;
       const isInEditMode = getRowMode(id) === GridRowModes.Edit;
