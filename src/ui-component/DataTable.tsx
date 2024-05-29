7e3b3e542eb1c4f2b711c3611e5b298797432277
Victor Winberg <victor.m.winberg@gmail.com>
2024-05-29 22:01:25 +0200

    feat(data-table): add actions column to DataTable component (#18)

--- src/ui-component/DataTable.tsx
+++ src/ui-component/DataTable.tsx
@@ -1,21 +1,26 @@
 import { useEffect } from 'react';
 
 // material-ui
-import { DataGrid, DataGridProps, GridRowModel, useGridApiRef } from '@mui/x-data-grid';
+import { DataGrid, DataGridProps, GridActionsCellItem, GridColDef, GridRowModel, GridRowModes, useGridApiRef } from '@mui/x-data-grid';
 
 // project imports
 import useWindowDimension from '@/hooks/useWindowDimension';
+
+// assets
+import { Cancel, Delete, Edit, Save } from '@mui/icons-material';
 
 // ==============================|| DATA TABLE ||============================== //
 
 interface DataTableProps extends DataGridProps {
   rows: GridRowModel[];
+  showActions?: boolean;
 }
 
-const DataTable = ({ columns, rows, ...rest }: DataTableProps) => {
+const DataTable = ({ columns, rows, showActions, ...rest }: DataTableProps) => {
   const apiRef = useGridApiRef();
-
   const [width] = useWindowDimension();
+
+  const actions = showActions ? [Actions(apiRef)] : [];
 
   useEffect(() => {
     apiRef.current.autosizeColumns({ expand: true });
@@ -24,11 +29,10 @@ const DataTable = ({ columns, rows, ...rest }: DataTableProps) => {
   return (
     <DataGrid
       apiRef={apiRef}
-      columns={columns}
+      columns={[...columns, ...actions]}
       rows={rows}
-      autosizeOnMount
-      autosizeOptions={{ expand: true }}
       autoPageSize
+      editMode="row"
       sx={{
         mx: -1,
         '&, [class^=MuiDataGrid]': { border: 'none' },
@@ -37,6 +41,9 @@ const DataTable = ({ columns, rows, ...rest }: DataTableProps) => {
         },
         '& .MuiDataGrid-cell:focus-within': {
           outline: 'none !important'
+        },
+        '& input': {
+          padding: '0 8px !important'
         }
       }}
       {...rest}
@@ -45,3 +52,47 @@ const DataTable = ({ columns, rows, ...rest }: DataTableProps) => {
 };
 
 export default DataTable;
+
+const Actions = (apiRef: ReturnType<typeof useGridApiRef>): GridColDef => {
+  return {
+    field: 'actions',
+    type: 'actions',
+    headerName: 'Actions',
+    cellClassName: 'actions',
+    getActions: ({ id }) => {
+      const { getRowMode, startRowEditMode, stopRowEditMode } = apiRef.current;
+      const isInEditMode = getRowMode(id) === GridRowModes.Edit;
+
+      if (isInEditMode) {
+        return [
+          <GridActionsCellItem
+            icon={<Save />}
+            label="Save"
+            sx={{
+              color: 'primary.main'
+            }}
+            onClick={() => stopRowEditMode({ id })}
+          />,
+          <GridActionsCellItem
+            icon={<Cancel />}
+            label="Cancel"
+            className="textPrimary"
+            onClick={() => stopRowEditMode({ id, ignoreModifications: true })}
+            color="inherit"
+          />
+        ];
+      }
+
+      return [
+        <GridActionsCellItem
+          icon={<Edit />}
+          label="Edit"
+          className="textPrimary"
+          onClick={() => startRowEditMode({ id })}
+          color="inherit"
+        />,
+        <GridActionsCellItem icon={<Delete />} label="Delete" onClick={console.log} color="inherit" />
+      ];
+    }
+  };
+};
