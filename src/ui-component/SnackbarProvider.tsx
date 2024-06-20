bda30862366a668e750abb2d4b62f1c5f6cf573a
Victor Winberg <victor.m.winberg@gmail.com>
2024-06-20 17:17:21 +0200

    feat(snackbar): handle multiple snackbars (#35)

--- src/ui-component/SnackbarProvider.tsx
+++ src/ui-component/SnackbarProvider.tsx
@@ -1,35 +1,51 @@
 import { Alert, AlertColor, Snackbar } from '@mui/material';
 import { FunctionComponent, PropsWithChildren, createContext, useState } from 'react';
 
+const TIMEOUT = 6000; // 6 seconds
+
 export interface SnackbarContextType {
   showSnackbar: (message: string, severity?: AlertColor) => void;
 }
 
 export const SnackbarContext = createContext<SnackbarContextType | undefined>(undefined);
 
+type Snackbar = { message: string; severity: AlertColor };
+
 const SnackbarProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
-  const [snackbar, setSnackbar] = useState({
-    open: false,
-    message: '',
-    severity: 'success' as AlertColor
-  });
+  const [snackbars, setSnackbars] = useState<Snackbar[]>([]);
+  const [open, setOpen] = useState(false);
+  const snackbar = snackbars[0] || { message: '', severity: 'success' };
 
   const showSnackbar = (message: string, severity: AlertColor = 'success') => {
-    setSnackbar({
-      open: true,
-      message,
-      severity
-    });
+    setSnackbars([...snackbars, { message, severity }]);
+    setOpen(!open);
   };
 
-  const handleClose = () => {
-    setSnackbar({ ...snackbar, open: false });
+  const handleClose = (_event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleExited = () => {
+    setSnackbars(snackbars.slice(1));
+
+    if (snackbars.length > 1) {
+      setOpen(true);
+    }
   };
 
   return (
     <SnackbarContext.Provider value={{ showSnackbar }}>
       {children}
-      <Snackbar open={snackbar.open} autoHideDuration={6000} onClose={handleClose}>
+      <Snackbar
+        open={open}
+        autoHideDuration={TIMEOUT}
+        onClose={handleClose}
+        TransitionProps={{ onExited: handleExited }}
+      >
         <Alert onClose={handleClose} severity={snackbar.severity}>
           {snackbar.message}
         </Alert>
